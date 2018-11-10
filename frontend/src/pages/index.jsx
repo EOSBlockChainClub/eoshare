079dbe1dc4580d6dabb70dd61bfe650d0b84305c
nabigraphics <hbh2692@naver.com>
2018-11-10 15:41:23 -0800

    Hello EOS Hackathon!

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Layout } from "components/layouts";
+class IndexPage extends Component {
+  render() {
+    return <Layout>Hello EOS Hackathon!</Layout>;
+  }
+}
+
+export default IndexPage;
